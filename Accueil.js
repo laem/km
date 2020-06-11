6c46d0c5929c0b6b5de28261fa339341c999b97d
Mael <mael.thomas@data.gouv.fr>
2020-06-11 21:00:57 +0200

    On vide tout pour repartir sur un autre site

--- Accueil.js
+++ Accueil.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import Article from './Article'
-import frontMatter from 'front-matter'
-import { imageResizer } from './Article'
-import { Switch, Route, BrowserRouter as Router, Link } from 'react-router-dom'
+import {
+	Switch,
+	Route,
+	BrowserRouter as Router,
+	Link,
+	useLocation,
+} from 'react-router-dom'
 import ScrollToTop from './ScrollToTop'
 
 export const pageLayout = `
@@ -10,29 +13,13 @@ export const pageLayout = `
 			margin: 0 auto;
 `
 
-var req = require.context('./articles', true, /\.md$/)
-const rawArticles = [...req.keys()]
-	.filter((key) => !key.includes('brouillon'))
-	.map((key) => [key.replace('./', '').replace('.md', ''), req(key).default])
-console.log({ rawArticles })
-
-export const parsedArticles = rawArticles.map(([id, string]) => ({
-	...frontMatter(string),
-	id,
-}))
-
 export default () => {
-	const path = decodeURI(window.location.pathname)
-
 	return (
 		<Router>
 			<ScrollToTop>
 				<Switch>
-					<Route path="/:id">
-						<Article />
-					</Route>
 					<Route path="/">
-						<Liste articles={parsedArticles} />
+						<Accueil />
 					</Route>
 					<Route path="*">
 						<NoMatch />
@@ -76,74 +63,7 @@ const Header = () => (
 	</header>
 )
 
-let Liste = ({ articles }) => (
-	<main css={pageLayout}>
-		<Header />
-		<section
-			css={`
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-				aside {
-					text-align: center;
-				}
-			`}
-		>
-			<header></header>
-			{articles
-				.sort((a1, a2) => (a1.attributes.date > a2.attributes.date ? -1 : 1))
-				.map((a) => (
-					<aside
-						css={`
-							p {
-								max-width: 36rem;
-								margin-bottom: 0.3rem;
-							}
-							header {
-								margin-bottom: 1rem;
-							}
-							header > a {
-								text-decoration: none;
-							}
-
-							h2 {
-								display: block;
-								text-decoration: none;
-								margin-bottom: 0.3rem;
-							}
-							margin-bottom: 3rem;
-						`}
-					>
-						<header>
-							<Link to={'/' + a.id}>
-								<h2>{a.attributes.titre}</h2>
-							</Link>
-							<small>
-								{a.attributes.date.toLocaleString(undefined, {
-									weekday: 'long',
-									year: 'numeric',
-									month: 'long',
-									day: 'numeric',
-								})}
-							</small>
-						</header>
-						<Link to={'/' + a.id}>
-							<img
-								css="width: 10rem; box-shadow: rgb(147, 143, 143) 2px 2px 10px 0px;"
-								src={imageResizer('m')(a.attributes.image)}
-							></img>
-						</Link>
-						<p>
-							{a.attributes.résumé}{' '}
-							<Link to={'/' + a.id}>
-								<em>Lire</em>
-							</Link>
-						</p>
-					</aside>
-				))}
-		</section>
-	</main>
-)
+const Accueil = () => <div>Salut</div>
 
 function NoMatch() {
 	let location = useLocation()
